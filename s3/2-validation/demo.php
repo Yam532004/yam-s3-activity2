8a5258dec0e788c04a4c6749e1775bb038eb5fd3
Y AM <yam532004gmail.com>
2023-12-07 09:21:34 +0700

    change

--- s3/2-validation/demo.php
+++ s3/2-validation/demo.php
@@ -2,29 +2,20 @@
 
 function validate_message($message)
 {
-    // function to check if message is correct (must have at least 10 characters (after trimming))
-    if (strlen(trim($message)) >= 10) {
-        return true;
-    }
-    return false;
+    // Function to check if the message is correct (must have at least 10 characters after trimming)
+    return strlen(trim($message)) >= 10;
 }
 
 function validate_username($username)
 {
-    // function to check if username is correct (must be alphanumeric => Use the function 'ctype_alnum()')
-    if (ctype_alnum($username)) {
-        return true;
-    }
-    return false;
+    // Function to check if the username is correct (must be alphanumeric)
+    return ctype_alnum($username);
 }
 
 function validate_email($email)
 {
-    // function to check if email is correct (must contain '@')
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        return true;
-    }
-    return false;
+    // Function to check if the email is correct (must contain '@')
+    return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
 }
 
 $user_error = "";
@@ -43,28 +34,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $message = $_POST['message'];
     $terms = isset($_POST['terms']);
 
+    // Validate username
     if (empty($username)) {
         $user_error = "Please enter a username";
     } elseif (!validate_username($username)) {
         $user_error = "Username should contain only letters and numbers";
     }
 
+    // Validate email
     if (empty($email)) {
         $email_error = "Please enter an email";
     } elseif (!validate_email($email)) {
         $email_error = "Email must contain '@'";
     }
 
-    if (empty($message)) {
-        $message_error = "Please enter your message";
-    } elseif (!validate_message($message)) {
+    // Validate message
+    if (empty($message) || !validate_message($message)) {
         $message_error = "Message must be at least 10 characters long";
     }
 
+    // Validate terms
     if (!$terms) {
         $terms_error = "You must accept the Terms of Service";
     }
 
+    // Check if all validations passed
     if (empty($user_error) && empty($email_error) && empty($message_error) && empty($terms_error)) {
         $form_valid = true;
     }
@@ -75,26 +69,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <form action="index.php" method="post">
     <div class="row mb-3 mt-3">
         <div class="col">
-            <input type="text" class="form-control" placeholder="Enter Name" name="username" value="<?php             
-            echo $username; ?>">
+            <input type="text" class="form-control" placeholder="Enter Name" name="username">
             <small class="form-text text-danger"> <?php echo $user_error; ?></small>
         </div>
         <div class="col">
-            <input type="text" class="form-control" placeholder="Enter email" name="email" value="<?php echo $email; ?>">
+            <input type="text" class="form-control" placeholder="Enter email" name="email">
             <small class="form-text text-danger"> <?php echo $email_error; ?></small>
         </div>
     </div>
     <div class="mb-3">
-        <textarea name="message" placeholder="Enter message" class="form-control"><?php echo $message; ?></textarea>
+        <textarea name="message" placeholder="Enter message" class="form-control"></textarea>
         <small class="form-text text-danger"> <?php echo $message_error; ?></small>
     </div>
     <div class="mb-3">
-        <input type="checkbox" class="form-control-check" name="terms" id="terms" value="1" <?php
-            $terms = isset($_POST['terms']);
-            if ($terms) {
-                echo 'checked';
-            } ?>>
-        <label for="terms">I accept the Terms of Service</label>
+        <input type="checkbox" class="form-control-check" name="terms" id="terms" value="terms"> <label for="terms">I accept the Terms of Service</label>
         <small class="form-text text-danger"> <?php echo $terms_error; ?></small>
     </div>
     <div class="d-grid">
@@ -104,7 +92,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <hr>
 
-<?php if ($form_valid) :
+<?php
+if ($form_valid) :
 ?>
     <div class="card">
         <div class="card-header">
--- s3/2-validation/index.php
+++ s3/2-validation/index.php
@@ -2,4 +2,3 @@
     require_once 'templates/header.php';
     require_once 'form.php'; 
     require_once 'templates/footer.php';
-
